61bc200b17bb863de9178469a3e90f2c2a2bc645
KREEAL <k-real1337@mail.ru>
2024-03-19 23:24:07 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,103 @@
+function mockExit() {
+  alert("Вы вышли");
+}
+
+class message {
+  constructor(sender, date, text) {
+    this.sender = sender;
+    this.date = date;
+    this.text = text;
+  }
+}
+
+let messages = [];
+
+let msg = null;
+
+for (let i = 0; i < 10; i++) {
+  msg = new message("Арсений", "31.08.99. 21:50", "Поздравляю с Новым Годом");
+  messages.push(msg);
+}
+
+function addMessageToScroll(date, message, fromFriend) {
+  let elem = document.getElementById("messagesscroll");
+  if (fromFriend == false) {
+    elem.innerHTML +=
+      '<div class="minemessage"><h4>' +
+      date +
+      "</h4><h3>" +
+      message +
+      "</h3></div>";
+    //прокрутка скролбара
+    elem.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+      inline: "nearest",
+    });
+  } else {
+    document.getElementById("messagesscroll").innerHTML +=
+      '<div class="friendmessage"><h4>' +
+      date +
+      "</h4><h3>" +
+      message +
+      "</h3></div>";
+    //прокрутка скролбара
+    elem.scrollTop = elem.scrollHeight;
+    scroll.animate({
+      scrollLeft: scroll.get()[0].scrollWidth,
+    });
+  }
+}
+
+const loginBtn = document.getElementById("login");
+const signupBtn = document.getElementById("signup");
+
+/*loginBtn.addEventListener("click", (e) => {
+  let parent = e.target.parentNode.parentNode;
+  Array.from(e.target.parentNode.parentNode.classList).find((element) => {
+    if (element !== "slide-up") {
+      parent.classList.add("slide-up");
+    } else {
+      signupBtn.parentNode.classList.add("slide-up");
+      parent.classList.remove("slide-up");
+    }
+  });
+});*/
+
+signupBtn.addEventListener("click", (e) => {
+  let parent = e.target.parentNode;
+  Array.from(e.target.parentNode.classList).find((element) => {
+    if (element !== "slide-up") {
+      parent.classList.add("slide-up");
+    } else {
+      loginBtn.parentNode.parentNode.classList.add("slide-up");
+      parent.classList.remove("slide-up");
+    }
+  });
+});
+
+function mockSendMessage() {
+  const input = document.getElementById("inputmessage");
+  if (input.value != "") {
+    const now = new Date();
+    addMessageToScroll(
+      now.getHours() + ":" + now.getMinutes(),
+      input.value,
+      false
+    );
+  }
+}
+
+document
+  .querySelector("#inputmessage")
+  .addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      mockSendMessage();
+    }
+  });
+
+function showtofa() {
+  document.getElementById("2fa_container").style.visibility = "visible";
+}
+
+new QRCode();
---
+++ script_api.js
@@ -0,0 +1,150 @@
+function register() {
+  let login = document.getElementById("registerlogin").value;
+  let password = document.getElementById("registerpassword").value;
+  let body = { username: login, password: password };
+  postData("api/v1/auth/register", body).then((data) => {
+    console.log(data);
+    if (data.detail == null) {
+      localStorage.setItem("access_token", data.access_token);
+      localStorage.setItem("refresh_token", data.refresh_token);
+      localStorage.setItem("token_type", "app_token");
+
+      setTimeout(() => {
+        console.log("ЖДУ QR");
+
+        // Жизнь полна неожиданностей... Но эта пауза точно не будет лишней
+        document.getElementById("2fa_container").style.visibility = "visible";
+        document.getElementById("signcontainer").remove();
+        getQR();
+      }, 200);
+    }
+  });
+}
+
+function login() {
+  let login = document.getElementById("loginlogin").value;
+  let password = document.getElementById("loginpassword").value;
+  let body = { username: login, password: password };
+  postData("api/v1/auth/login", body).then((data) => {
+    console.log(data);
+
+    if (data.detail == null) {
+      localStorage.setItem("access_token", data.access_token);
+      localStorage.setItem("refresh_token", data.refresh_token);
+      localStorage.setItem("token_type", "pre_2fa_token");
+
+      setTimeout(() => {
+        console.log("ЖДУ ВВОД цифар");
+        document.getElementById("qrcode").remove();
+        document.getElementById("2fa_container").style.height = "300px";
+        document.getElementById("2fa_container").style.visibility = "visible";
+
+        document.getElementById("signcontainer").remove();
+        // Жизнь полна неожиданностей... Но эта пауза точно не будет лишней
+      }, 1);
+    }
+  });
+}
+
+function getQR() {
+  getData("api/v1/auth/my_totp_qr").then((response) => {
+    printQR(response.qr_text);
+  });
+}
+
+function checkTestTOTP() {
+  totpres = "";
+  totpres += document.getElementById("totp1").value;
+  totpres += document.getElementById("totp2").value;
+  totpres += document.getElementById("totp3").value;
+  totpres += document.getElementById("totp4").value;
+  totpres += document.getElementById("totp5").value;
+  totpres += document.getElementById("totp6").value;
+
+  console.log("testtotp");
+  console.log(localStorage.getItem("token_type"));
+
+  if (localStorage.getItem("token_type") == "pre_2fa_token") {
+    postData("api/v1/auth/login_totp", { code: totpres }).then(
+      (data, response) => {
+        console.log(data);
+        if (data.detail == null) {
+          localStorage.setItem("access_token", data.access_token);
+          localStorage.setItem("refresh_token", data.refresh_token);
+          localStorage.setItem("token_type", "app_token");
+          document.location.replace("/index.html");
+          console.log("Я ТУТ НОВЕНЬКИЙ");
+        }
+      }
+    );
+  } else if (localStorage.getItem("token_type") == "app_token") {
+    postData("/api/v1/auth/test_totp", { code: totpres }).then((data) => {
+      console.log(data);
+      if (data.success) {
+        document.location.replace("/index.html");
+      }
+    });
+  } else {
+    console.log("Все очень плохо");
+  }
+}
+
+// Example POST method implementation:
+async function getData(url = "") {
+  url = "https://chat-back.kekdev.top/" + url;
+  // Default options are marked with *
+
+  let headers = { "Content-Type": "application/json" };
+
+  if (localStorage.getItem("access_token") != null) {
+    const auth_header = "Bearer " + localStorage.getItem("access_token");
+    headers["Authorization"] = auth_header;
+  }
+
+  const response = await fetch(url, {
+    method: "GET", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: headers,
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+  });
+  response["_status"] = response.status;
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+// Example POST method implementation:
+async function postData(url = "", data = {}) {
+  url = "https://chat-back.kekdev.top/" + url;
+
+  let headers = { "Content-Type": "application/json" };
+
+  if (localStorage.getItem("access_token") != null) {
+    const auth_header = "Bearer " + localStorage.getItem("access_token");
+    headers["Authorization"] = auth_header;
+  }
+
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: headers,
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+function printQR(qr_text) {
+  var qrcode = new QRCode("qrcode_code_img", {
+    text: qr_text,
+    width: 300,
+    height: 300,
+    colorDark: "#000000",
+    colorLight: "#ffffff",
+  });
+}

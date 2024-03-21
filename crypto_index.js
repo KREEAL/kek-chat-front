e78a95503b003c2c8ebe32ed93d26a1594b8a814
KREEAL <k-real1337@mail.ru>
2024-03-21 20:37:14 +0300

    Сделано много всего

--- crypto_index.js
+++ crypto_index.js
@@ -28,9 +28,12 @@ async function createAndSaveKeyPairForUserChatConversationWithLocalStorage(
   const exportedAsBase64 = window.btoa(ab2str(exportedPubRaw));
   const exportedPrivateAsBase64 = window.btoa(ab2str(exportedPrivateRaw));
 
-  localStorage.setItem(user_id_to_chat_with + "_public", exportedAsBase64);
   localStorage.setItem(
-    user_id_to_chat_with + "_private",
+    user_id + "_" + user_id_to_chat_with + "_public",
+    exportedAsBase64
+  );
+  localStorage.setItem(
+    user_id + "_" + user_id_to_chat_with + "_private",
     exportedPrivateAsBase64
   );
 
@@ -41,4 +44,124 @@ async function createAndSaveKeyPairForUserChatConversationWithLocalStorage(
     document.getElementById("inputuserfinder").value = "";
     getAllConversations();
   });
+}
+
+async function createAndSaveKeyPairForComplete(
+  user_id_to_chat_with,
+  conversation_id
+) {
+  let keyPair = await window.crypto.subtle.generateKey(
+    {
+      name: "RSA-OAEP",
+      modulusLength: 2048,
+      publicExponent: new Uint8Array([1, 0, 1]),
+      hash: "SHA-256",
+    },
+    true,
+    ["encrypt", "decrypt"]
+  );
+  let exportedPubRaw = await window.crypto.subtle.exportKey(
+    "spki",
+    keyPair.publicKey
+  );
+
+  let exportedPrivateRaw = await window.crypto.subtle.exportKey(
+    "pkcs8",
+    keyPair.privateKey
+  );
+
+  const exportedAsBase64 = window.btoa(ab2str(exportedPubRaw));
+  const exportedPrivateAsBase64 = window.btoa(ab2str(exportedPrivateRaw));
+
+  localStorage.setItem(
+    user_id + "_" + user_id_to_chat_with + "_public",
+    exportedAsBase64
+  );
+  localStorage.setItem(
+    user_id + "_" + user_id_to_chat_with + "_private",
+    exportedPrivateAsBase64
+  );
+
+  postData("api/v1/conversations/" + conversation_id + "/complete", {
+    public_key: exportedAsBase64,
+  }).then((data) => {
+    document.getElementById("inputuserfinder").value = "";
+    getAllConversations();
+  });
+}
+
+async function encryptMyMessage(msg_text, user_id_chat) {
+  const pemContents = localStorage.getItem(
+    user_id + "_" + user_id_chat + "_public_alien"
+  );
+  // base64 decode the string to get the binary data
+  const binaryDerString = window.atob(pemContents);
+  // convert from a binary string to an ArrayBuffer
+  const binaryDer = str2ab(binaryDerString);
+
+  const alien_public_key = await window.crypto.subtle.importKey(
+    "spki",
+    binaryDer,
+    {
+      name: "RSA-OAEP",
+      hash: "SHA-256",
+    },
+    true,
+    ["encrypt"]
+  );
+
+  let enc = new TextEncoder();
+  let encoded = enc.encode(msg_text);
+
+  enc_text = await window.crypto.subtle.encrypt(
+    {
+      name: "RSA-OAEP",
+    },
+    alien_public_key,
+    encoded
+  );
+
+  return window.btoa(ab2str(enc_text));
+}
+
+async function decryptAlienMessage(encrypted_msg_text, user_id_chat) {
+  const pemContents = localStorage.getItem(
+    user_id + "_" + user_id_chat + "_private"
+  );
+  // base64 decode the string to get the binary data
+  const binaryDerString = window.atob(pemContents);
+  // convert from a binary string to an ArrayBuffer
+  const binaryDer = str2ab(binaryDerString);
+  try {
+    const my_private_key = await window.crypto.subtle.importKey(
+      "pkcs8",
+      binaryDer,
+      {
+        name: "RSA-OAEP",
+        hash: "SHA-256",
+      },
+      true,
+      ["decrypt"]
+    );
+    console.log("555");
+
+    decrypted_txt = await window.crypto.subtle.decrypt(
+      { name: "RSA-OAEP" },
+      my_private_key,
+      str2ab(window.atob(encrypted_msg_text))
+    );
+    return decrypted_txt;
+  } catch (e) {
+    console.log("123");
+    console.log(e);
+  }
+}
+
+function str2ab(str) {
+  const buf = new ArrayBuffer(str.length);
+  const bufView = new Uint8Array(buf);
+  for (let i = 0, strLen = str.length; i < strLen; i++) {
+    bufView[i] = str.charCodeAt(i);
+  }
+  return buf;
 }
--- indexscript.js
+++ indexscript.js
@@ -1,6 +1,9 @@
 user_id = null;
 user_name = "";
 users_ids_i_have_chat_with = [];
+current_conversation_id = null;
+current_user_chat_id = null;
+current_chat_messages = [];
 
 function getCurrentUser() {
   getData("api/v1/users/me").then((data) => {
@@ -31,6 +34,14 @@ function get_conversation_symbol(conversation) {
   }
 }
 
+function whichUserSentDialogue(element) {
+  if (element.user_1_pub_key != null) {
+    return element.user_1_id;
+  } else {
+    return element.user_2_id;
+  }
+}
+
 function getAllConversations() {
   document.getElementById("peopleScroll").innerHTML = "";
 
@@ -46,6 +57,8 @@ function getAllConversations() {
             conversation_id: element.conversation_id,
             user_id: element.user_2_obj.user_id,
             user_username: element.user_2_obj.username,
+            initialized: element.initialized,
+            sender: whichUserSentDialogue(element),
           });
 
           document.getElementById("peopleScroll").innerHTML +=
@@ -61,6 +74,8 @@ function getAllConversations() {
             conversation_id: element.conversation_id,
             user_id: element.user_1_obj.user_id,
             user_username: element.user_1_obj.username,
+            initialized: element.initialized,
+            sender: whichUserSentDialogue(element),
           });
 
           document.getElementById("peopleScroll").innerHTML +=
@@ -110,20 +125,129 @@ function click_on_user_button(callingObject) {
   user_on_click_id = getConversationID(callingObject);
 
   already_dialogue = false;
+  is_initialized = false;
+  sender_id = null;
+  conversation_id = null;
+  user_id_to_chat = null;
 
   users_ids_i_have_chat_with.forEach((element) => {
     if (element.user_id == user_on_click_id) {
       already_dialogue = true;
+      is_initialized = element.initialized;
+      sender_id = element.sender;
+      conversation_id = element.conversation_id;
+      user_id_to_chat = element.user_id;
     }
   });
 
   if (already_dialogue) {
-    console.log("тут вызывать комплит, если ко мне пришел запрос");
+    if (is_initialized) {
+      console.log("Переход в чат");
+      open_chat_with_user(user_id_to_chat, conversation_id);
+    } else if (sender_id == user_id) {
+      console.log("Я отправил запрос на чат");
+    } else {
+      console.log("Мне отправили запрос на чат");
+      completeChatCreation(sender_id, conversation_id);
+    }
   } else {
     createAndSaveKeyPairForUserChatConversationWithLocalStorage(
       user_on_click_id
     );
   }
+}
+
+function completeChatCreation(user_id_to_chat, conversation_id) {
+  createAndSaveKeyPairForComplete(user_id_to_chat, conversation_id);
+  console.log("Переход в чат");
+}
+
+function open_chat_with_user(open_chat_with_me_id, conversation_id) {
+  conversation_id = null;
+
+  //замена ника
+  username_of_chatting_user = null;
+  users_ids_i_have_chat_with.forEach((element) => {
+    if (element.user_id == open_chat_with_me_id) {
+      username_of_chatting_user = element.user_username;
+      conversation_id = element.conversation_id;
+    }
+  });
+
+  current_conversation_id = conversation_id;
+  current_user_chat_id = open_chat_with_me_id;
+
+  document.getElementById("nameofchattinguser").innerHTML =
+    username_of_chatting_user;
+
+  //получение и сохранение чужого ключа
+
+  getData("api/v1/conversations/" + conversation_id).then((data) => {
+    if (data.user_1_id == open_chat_with_me_id) {
+      //если первый узер - тот, с кем я открыл чат
+      user_chating_public_key = data.user_1_pub_key;
+      localStorage.setItem(
+        user_id + "_" + user_id_to_chat + "_public_alien",
+        user_chating_public_key
+      );
+    } else {
+      user_chating_public_key = data.user_2_pub_key;
+      localStorage.setItem(
+        user_id + "_" + user_id_to_chat + "_public_alien",
+        user_chating_public_key
+      );
+    }
+  });
+
+  //очистка чата
+  document.getElementById("messagesscroll").innerHTML = "";
+
+  getLastMessagesInChat();
+
+  console.log("получить чат");
+}
+
+async function sendMessage() {
+  text = document.getElementById("inputmessage").value;
+
+  encrypted_message = await encryptMyMessage(text, current_user_chat_id);
+  postData("api/v1/conversations/" + current_conversation_id + "/sendmsg", {
+    encrypted_text: encrypted_message,
+  });
+  document.getElementById("inputmessage").value = "";
+}
+
+function getLastMessagesInChat() {
+  getData("api/v1/conversations/" + current_conversation_id + "/messages").then(
+    (data) => {
+      data.forEach(async (element) => {
+        date = new Date(Date.parse(element.send_datetime));
+        current_chat_messages.push(element.message_id);
+
+        text = await decryptAlienMessage(
+          element.encrypted_text,
+          current_user_chat_id
+        );
+        fromFriend = null;
+        if (element.sender_id == current_user_chat_id) {
+          fromFriend = true;
+        } else {
+          fromFriend = false;
+        }
+        addMessageToScroll(
+          "·" +
+            date.getDate() +
+            date.getMonth() +
+            "," +
+            date.getHours() +
+            ":" +
+            date.getMinutes(),
+          text,
+          fromFriend
+        );
+      });
+    }
+  );
 }
 
 window.onload = () => {

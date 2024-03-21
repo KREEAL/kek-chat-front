542aeef18fdc2adbf29aaa1544f5f3af0c83428d
KREEAL <k-real1337@mail.ru>
2024-03-21 13:50:25 +0300

    Добавлены запросы на то чтобы початиться

--- crypto_index.js
+++ crypto_index.js
@@ -1,27 +1,44 @@
-let keyPair = await window.crypto.subtle.generateKey(
-  {
-    name: "RSA-OAEP",
-    modulusLength: 2048,
-    publicExponent: new Uint8Array([1, 0, 1]),
-    hash: "SHA-256",
-  },
-  true,
-  ["encrypt", "decrypt"]
-);
-
 function ab2str(buf) {
   return String.fromCharCode.apply(null, new Uint8Array(buf));
 }
 
-let exportedPubRaw = await window.crypto.subtle.exportKey(
-  "spki",
-  keyPair.publicKey
-);
+async function createAndSaveKeyPairForUserChatConversationWithLocalStorage(
+  user_id_to_chat_with
+) {
+  let keyPair = await window.crypto.subtle.generateKey(
+    {
+      name: "RSA-OAEP",
+      modulusLength: 2048,
+      publicExponent: new Uint8Array([1, 0, 1]),
+      hash: "SHA-256",
+    },
+    true,
+    ["encrypt", "decrypt"]
+  );
+  let exportedPubRaw = await window.crypto.subtle.exportKey(
+    "spki",
+    keyPair.publicKey
+  );
 
-let exportedPrivateRaw = await window.crypto.subtle.exportKey(
-  "spki",
-  keyPair.privateKey
-);
+  let exportedPrivateRaw = await window.crypto.subtle.exportKey(
+    "pkcs8",
+    keyPair.privateKey
+  );
 
-const exportedAsBase64 = window.btoa(ab2str(exportedPubRaw));
-const exportedPrivateAsBase64 = window.btoa(ab2str(exportedPrivateRaw));
+  const exportedAsBase64 = window.btoa(ab2str(exportedPubRaw));
+  const exportedPrivateAsBase64 = window.btoa(ab2str(exportedPrivateRaw));
+
+  localStorage.setItem(user_id_to_chat_with + "_public", exportedAsBase64);
+  localStorage.setItem(
+    user_id_to_chat_with + "_private",
+    exportedPrivateAsBase64
+  );
+
+  postData("api/v1/conversations", {
+    recipient_id: user_id_to_chat_with,
+    public_key: exportedAsBase64,
+  }).then((data) => {
+    document.getElementById("inputuserfinder").value = "";
+    getAllConversations();
+  });
+}
--- indexscript.js
+++ indexscript.js
@@ -1,5 +1,6 @@
-user_id = "";
+user_id = null;
 user_name = "";
+users_ids_i_have_chat_with = [];
 
 function getCurrentUser() {
   getData("api/v1/users/me").then((data) => {
@@ -9,23 +10,63 @@ function getCurrentUser() {
   });
 }
 
+// ▶ ⬤
+function get_conversation_symbol(conversation) {
+  if (conversation.initialized) {
+    return "";
+  }
+
+  if (conversation.user_1_id == user_id) {
+    if (conversation.user_1_pub_key == null) {
+      return "⬤";
+    } else {
+      return "▶";
+    }
+  } else {
+    if (conversation.user_2_pub_key == null) {
+      return "⬤";
+    } else {
+      return "▶";
+    }
+  }
+}
+
 function getAllConversations() {
+  document.getElementById("peopleScroll").innerHTML = "";
+
   getData("api/v1/conversations").then((data) => {
+    users_ids_i_have_chat_with = [];
     if (data.length == 0) {
       document.getElementById("peopleScroll").innerHTML +=
         '<h3 class="humantochat">Нет чатов</h3>';
     } else {
       data.forEach((element) => {
         if (element.user_1_id == user_id) {
+          users_ids_i_have_chat_with.push({
+            conversation_id: element.conversation_id,
+            user_id: element.user_2_obj.user_id,
+            user_username: element.user_2_obj.username,
+          });
+
           document.getElementById("peopleScroll").innerHTML +=
-            '<div class="humantochatdiv"><button class="chatbutton" onclick="getConversationID(this)"><h3 class="humantochat">' +
+            '<div class="humantochatdiv"><button class="chatbutton" onclick="click_on_user_button(this)"><h3 class="humantochat"> ' +
+            get_conversation_symbol(element) +
+            " " +
             element.user_2_obj.username +
             '</h3><h3 style="visibility:hidden">' +
             element.user_2_obj.user_id +
             "</h3></button></div>";
         } else {
+          users_ids_i_have_chat_with.push({
+            conversation_id: element.conversation_id,
+            user_id: element.user_1_obj.user_id,
+            user_username: element.user_1_obj.username,
+          });
+
           document.getElementById("peopleScroll").innerHTML +=
-            '<div class="humantochatdiv"><button class="chatbutton" onclick="getConversationID(this)" ><h3 class="humantochat">' +
+            '<div class="humantochatdiv"><button class="chatbutton" onclick="click_on_user_button(this)" ><h3 class="humantochat"> ' +
+            get_conversation_symbol(element) +
+            " " +
             element.user_1_obj.username +
             '</h3><h3 style="visibility:hidden">' +
             element.user_1_obj.user_id +
@@ -47,7 +88,7 @@ function searchonchange(value) {
       } else {
         data.forEach((element) => {
           document.getElementById("peopleScroll").innerHTML +=
-            '<div class="humantochatdiv" style = "background:#f0f0f0"><button class="chatbutton" onclick="getConversationID(this)"><h3 class="humantochat">' +
+            '<div class="humantochatdiv" style = "background:#f0f0f0"><button class="chatbutton" onclick="click_on_user_button(this)"><h3 class="humantochat">' +
             element.username +
             '</h3><h3 style="visibility:hidden">' +
             element.user_id +
@@ -61,8 +102,28 @@ function searchonchange(value) {
 }
 
 function getConversationID(callingObject) {
-  console.log(callingObject.children[1].innerHTML);
+  //console.log(callingObject.children[1].innerHTML);
   return callingObject.children[1].innerHTML;
+}
+
+function click_on_user_button(callingObject) {
+  user_on_click_id = getConversationID(callingObject);
+
+  already_dialogue = false;
+
+  users_ids_i_have_chat_with.forEach((element) => {
+    if (element.user_id == user_on_click_id) {
+      already_dialogue = true;
+    }
+  });
+
+  if (already_dialogue) {
+    console.log("тут вызывать комплит, если ко мне пришел запрос");
+  } else {
+    createAndSaveKeyPairForUserChatConversationWithLocalStorage(
+      user_on_click_id
+    );
+  }
 }
 
 window.onload = () => {
